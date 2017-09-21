a7b926cc4fce2e705b6fbe5fc78d7c00a305c151
LSinzker <luisasf1408@gmail.com>
2017-09-21 13:26:04 -0300

    Fixed minor errors in 'Compiler'

--- Q2/Compiler.hs
+++ Q2/Compiler.hs
@@ -2,16 +2,16 @@ module Compiler where
 
 import RBL as R
 import Lambda as L
-import standardLib as S
+import StandardLib as S
 
 translate :: R.Expression -> L.Term
 translate (R.B True) = S.true
 translate (R.B False) = S.false
 translate (R.N 0) = S.false
-translate (R.N m) = S.eval(S.scc(translate(N (m-1))))
+translate (R.N m) = L.eval(L.App S.scc (translate(N (m-1))))
 translate (R.Add e1 e2) = L.eval(L.App(L.App S.plus (translate e1))(translate e2))
-translate (R.Sub e1 e2) = L.eval(L.App(L.App S.plus (translate e1))(translate e2))
-translate (R.And e1 e2) = L.eval(L.App S.ands(L.App (translate d1) (translate d2)))
+translate (R.Sub e1 e2) = L.eval(L.App(L.App S.subt (translate e1))(translate e2))
+translate (R.And e1 e2) = L.eval(L.App S.ands(L.App (translate e1) (translate e2)))
 translate (R.Or e1 e2) = L.eval(L.App(L.App S.ors (translate e1))(translate e2))
 translate (R.Not e1) = L.eval(L.App S.nots (translate e1))
 translate (R.IfThenElse c e1 e2) = L.eval(L.App(L.App(L.App S.ite (translate c))(translate e1))(translate e2))
